d2712ee2b204582804dd63c1ba7bc08b4130fce3
Jayans Paliwal <jayans2002@hotmail.com>
2023-10-02 17:07:33 +0530

    fixed-escape-characters

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -37,7 +37,7 @@ export default function Intro() {
         </div>
       </div>
       <motion.h1 className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl' initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
-        <span className="font-bold">Hello, I'm Jayans Paliwal.</span> I'm a{" "}
+        <span className="font-bold">Hello, I&apos;m Jayans Paliwal.</span> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> and a recent college graduate. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
